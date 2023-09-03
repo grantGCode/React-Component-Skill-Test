d4660ab7f515e1f4f5a93bbbca3c49642da2cad7
Grant Bentley <gbentley0618@comcast.net>
2023-09-03 18:41:37 -0500

    First commit for skill test`

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Greating from './components/Text'
+
+function App() {
+  return (
+    <div> 
+      <Greating text='Hello World'/>
+    </div>
+  );
+}
+
+export default App;
